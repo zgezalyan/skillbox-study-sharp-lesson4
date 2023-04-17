29de43cb95b65878d5b4d74f945b57d06afc8456
Zurab Gezalyan <mcdoss@mail.ru>
2023-04-18 01:32:38 +0300

    Exercise complete

---
+++ ArrayConsoleApp/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace ArrayConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rowSize;
+            int colSize;
+            int matrixASum = 0;
+            int matrixBSum = 0;
+            int resMatrixSum = 0;
+
+            int[,] matrixA;
+            int[,] matrixB;
+            int[,] sumOfMatrix;
+
+            Random rand = new Random();
+
+            Console.Write("Введите количество строк: ");
+            rowSize = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите количество столбцов: ");
+            colSize = int.Parse(Console.ReadLine());
+
+            matrixA = new int[rowSize, colSize];
+            matrixB = new int[rowSize, colSize];
+            sumOfMatrix = new int[rowSize, colSize];
+            
+            Console.WriteLine("Матрица А:");
+            for (int i = 0; i < rowSize; i++)
+            {
+                for (int j = 0; j < colSize; j++)
+                {
+                    matrixA[i, j] = rand.Next(100);
+                    matrixB[i, j] = rand.Next(100);
+                    sumOfMatrix[i, j] = matrixA[i, j] + matrixB[i, j];
+                    matrixASum += matrixA[i, j];
+                    matrixBSum += matrixB[i, j];
+                    resMatrixSum += sumOfMatrix[i, j];
+                    Console.Write($"{matrixA[i, j],5}");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine($"Сумма всех элементов матрицы А: {matrixASum}");
+            Console.WriteLine();
+
+            Console.WriteLine("Матрица B:");
+            for (int i = 0; i < rowSize; i++)
+            {
+                for (int j = 0; j < colSize; j++)
+                {
+                    Console.Write($"{matrixB[i, j],5}");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine($"Сумма всех элементов матрицы B: {matrixBSum}");
+            Console.WriteLine();
+
+            Console.WriteLine("Сумма матриц А и B:");
+            for (int i = 0; i < rowSize; i++)
+            {
+                for (int j = 0; j < colSize; j++)
+                {
+                    Console.Write($"{sumOfMatrix[i, j],5}");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine($"Сумма всех элементов матрицы: {resMatrixSum}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LifeSim/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+
+namespace LifeSim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool[,] cellBoolArray = new bool[25, 50];
+            string[,] cellShowArray = new string[25, 50];
+            int[,] cellNeighborsArray = new int[25, 50];
+
+            Random rand = new Random();
+            for (int i = 0; i < cellBoolArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < cellShowArray.GetLength(1); j++)
+                {
+                    cellBoolArray[i, j] = rand.Next() % 2 == 0;
+                }
+            }
+
+            Console.CursorVisible = false;
+            while (true)
+            {
+                // Цикл подсчета соседей
+                for (int i = 0; i < cellBoolArray.GetLength(0); i++)
+                {
+                    for (int j = 0; j < cellShowArray.GetLength(1); j++)
+                    {
+                        cellNeighborsArray[i, j] = 0;
+                        if (i > 0)
+                        {
+                            if (cellBoolArray[i - 1, j])
+                            {
+                                cellNeighborsArray[i, j] += 1;
+                            }
+                            if (j > 0)
+                            {
+                                if (cellBoolArray[i - 1, j - 1])
+                                {
+                                    cellNeighborsArray[i, j] += 1;
+                                }
+                            }
+                            if (j < cellBoolArray.GetLength(1) - 1)
+                            {
+                                if (cellBoolArray[i - 1, j + 1])
+                                {
+                                    cellNeighborsArray[i, j] += 1;
+                                }
+                            }
+
+                        }
+                        if (i < cellBoolArray.GetLength(0) - 1)
+                        {
+                            if (cellBoolArray[i + 1, j])
+                            {
+                                cellNeighborsArray[i, j] += 1;
+                            }
+                            if (j > 0)
+                            {
+                                if (cellBoolArray[i + 1, j - 1])
+                                {
+                                    cellNeighborsArray[i, j] += 1;
+                                }
+                            }
+                            if (j < cellBoolArray.GetLength(1) - 1)
+                            {
+                                if (cellBoolArray[i + 1, j + 1])
+                                {
+                                    cellNeighborsArray[i, j] += 1;
+                                }
+                            }
+                        }
+                        if (j > 0)
+                        {
+                            if (cellBoolArray[i, j - 1])
+                            {
+                                cellNeighborsArray[i, j] += 1;
+                            }
+                        }
+                        if (j < cellBoolArray.GetLength(1) - 1)
+                        {
+                            if (cellBoolArray[i, j + 1])
+                            {
+                                cellNeighborsArray[i, j] += 1;
+                            }
+                        }
+                    }
+
+                }
+
+                // Цикл расчета жизни в клетках
+                for (int i = 0; i < cellBoolArray.GetLength(0); i++)
+                {
+                    for (int j = 0; j < cellShowArray.GetLength(1); j++)
+                    {
+                        if (cellBoolArray[i, j])
+                        {
+                            if (cellNeighborsArray[i, j] < 2 || cellNeighborsArray[i, j] > 3)
+                            {
+                                cellBoolArray[i, j] = false;
+                            }
+                        }
+                        else
+                        {
+                            if (cellNeighborsArray[i, j] == 3)
+                            {
+                                cellBoolArray[i, j] = true;
+                            }
+                        }
+                        cellShowArray[i, j] = cellBoolArray[i, j] ? "O" : "-";
+                        Console.Write($"{cellShowArray[i, j],2}");
+                    }
+                    Console.WriteLine();
+                }
+                if (Console.ReadKey().KeyChar == 'q')
+                {
+                    break;
+                }
+                Console.Clear();
+            }
+        }
+    }
+}
